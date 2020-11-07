9c320603a15b640f4fbc9ef5a858dc6400d9c42d
sborys00 <16275683+sborys00@users.noreply.github.com>
2020-11-07 19:11:22 +0100

    add NewsWebsite class

---
+++ DiscordNewsBot/Models/NewsWebsite.cs
@@ -0,0 +1,9 @@
+﻿namespace DiscordNewsBot.Models
+{
+    class NewsWebsite
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public int Color { get; set; }
+    }
+}
